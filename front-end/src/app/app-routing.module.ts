9ed2e31f7cb4bafea38df661e38f87a2a9ad1758
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-02-03 21:54:09 -0800

    set router outlet on app component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
 	{
 		path: '',
-		component: AppComponent
+		component: HomeComponent
 	},
 	{
 		path: 'register',
