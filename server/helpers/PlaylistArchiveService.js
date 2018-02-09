abcf6002e44b2c6ea6ba98db567c0008e3b709bc
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-02-08 22:11:02 -0800

    Add stubs for handling functions

--- server/helpers/PlaylistArchiveService.js
+++ server/helpers/PlaylistArchiveService.js
@@ -4,33 +4,65 @@ import SpotifyInterface from '../helpers/SpotifyInterface';
 var redisInterface;
 var spotifyInterface;
 
+
+
+function _updatePlaylist(userId) {
+
+}
+
+/*
+  The token has expired, refresh the token, return the new accessToken
+*/
+function _handleUnauthorized(refreshToken) {
+
+}
+
+function _handleOtherErrors(error) {
+
+}
+
+
+
 class PlaylistArchiveService {
   constructor() {
     redisInterface = new RedisInterface();
     spotifyInterface = new SpotifyInterface();
   }
 
-  tryUpdatePlaylist(userId) {
+  updatePlaylist(userId) {
     redisInterface.getUserSpotifyTokens('andrew85.lloyd@gmail.com')
       .then(tokens => {
-        spotifyInterface.getPlaylistIds(tokens.accessToken.replace('a','b'))
-        .catch(err => {
-          if (err.message === 'Unauthorized') {
-            
-          }
+        return new Promise((resolve, reject) => {
+          spotifyInterface.getPlaylistIds(tokens.accessToken)
+          .catch(err => {
+            if (err.message === 'Unauthorized') {
+              reject({retry: true})
+            } else {
+              reject({retry: false})
+            }
+          })
+          .then(playlistIds => {
+            resolve(playlistIds)
+          })
+        })
+        .then(d => {
+          console.log(d)
+        })
+        .catch(e => {
+          console.log(e)
         })
       })
-      .then(playlistIds => {
-        console.log('playlistIDs', playlistIds)
-      })
-
   }
 }
 
 export default PlaylistArchiveService;
 
+// offer a function that will try to update the playlist. If it errors for an Unauthorized
+// refresh the token, and then try again. If it error for anything else, handle it like a
+// normal error
+
 const playlistArchiveService = new PlaylistArchiveService();
-playlistArchiveService.tryUpdatePlaylist()
+playlistArchiveService.updatePlaylist()
 /*function refreshToken(userId) {
   redisInterface.getUserSpotifyTokens(userId)
   .then(tokens => {
