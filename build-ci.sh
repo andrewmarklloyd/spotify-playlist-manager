1705df7878349bda3b0bd0738129eaa0c2506f9f
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-05-28 18:55:39 -0700

    Update build

--- build-ci.sh
+++ build-ci.sh
@@ -4,12 +4,14 @@ function buildApiImage {
 	echo 'Building API Docker image'
 	IMAGE="andrewlloyd/spotify-playlist-app"
 
+	npm run build
+
 	docker build -t $IMAGE .
 
 	# must be part the organization
-	#echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
+	echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-	#docker push $IMAGE
+	docker push $IMAGE
 }
 
 buildApiImage
